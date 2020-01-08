2b33eaa31a9fcb83e5e2018ba231c1dfe88ce57d
pngnwthbrd <coding@pngnwthbrd.com>
2020-01-08 22:40:59 +0100

    linux compatibility

--- main.c
+++ main.c
@@ -2,54 +2,62 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-#define MAX_NUMBERS 20
 
 int main()
-{
-    const char alphabet[31] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-                         'z', '$', '*', '(', ')', '#'};
+        {
+        const char alphabet[31] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                                'z', '$', '*', '(', ')', '#'};
 
-    while (1 == 1)
-    {
+        while (1 == 1)
+        {
         srand(time(NULL));
         unsigned int n, c;
         char  str;
-        char mix[2];
-        char pass[MAX_NUMBERS] = "";
+        char mix[13];
+        char *pass;
 
-        const unsigned int pass_size;
+        unsigned int pass_size;
 
-        printf("Please enter password size (max size: %d) or 'e' to exit: ", MAX_NUMBERS);
+        printf("Please enter password size or 'e' to exit: ");
         scanf("%d", &pass_size);
 
-        for (int i = 0; i < MAX_NUMBERS/2; i++)
-        {
-            n = rand() % 10;
-            str = alphabet[rand() % 31];
-            c = rand() % 2;
+        pass = (char *) malloc(pass_size * sizeof(char));
 
-            if (c == 0)
-                sprintf(mix, "%d%c", n, str);
-            else
-                sprintf(mix, "%c%d", str, n);
+        if (pass == NULL)
+                {
+                printf("Not enough free memory to calculate the password :(!");
+                return 0;
+                }
 
-            strcat(pass, mix);
-        }
+        for (int i = 0; i <= pass_size/2; i++)
+                {
+                n = rand() % 10;
+                str = alphabet[rand() % 31];
+                c = rand() % 2;
 
-        char return_pass[] = "";
+                if (c == 0)
+                        sprintf(mix, "%d%c", n, str);
+                else
+                        sprintf(mix, "%c%d", str, n);
+
+                strcat(pass, mix);
+                }
+
+        char return_pass[] = "\0";
         strncat(return_pass, pass, pass_size);
 
         char exit;
         scanf("%c", &exit);
 
         if (exit != '\n' && exit != 'e')
-            continue;
+                continue;
         else if (exit == 'e')
-            break;
+                break;
 
-        printf("Your random password: %s: \n\n",  return_pass);
+        printf("Your random password: %s \n\n",  return_pass);
 
-    }
-
+        if (pass != NULL)
+                free(pass);
+        }
 }
